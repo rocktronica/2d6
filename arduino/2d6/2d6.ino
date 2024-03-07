ea48cdd3d6e3b083f7a28915b3c48daef2f16b19
Tommy <tommy@iamnotagoodartist.com>
2024-03-07 15:01:26 -0800

    Swap printout for graph

--- arduino/2d6/2d6.ino
+++ arduino/2d6/2d6.ino
@@ -8,13 +8,13 @@ ArduboyTones sound(arduboy.audio.enabled);
 Tinyfont tinyfont = Tinyfont(arduboy.sBuffer, WIDTH, HEIGHT);
 
 uint8_t currentRollValues[2] = {0, 0};
-int pastRollValues[11] = {0,0,0,0,0,0,0,0,0,0,0};
+int sumCounts[11] = {0,0,0,0,0,0,0,0,0,0,0};
 
 void update() {
   currentRollValues[0] = random(0, 6 + 1);
   currentRollValues[1] = random(0, 6 + 1);
 
-  pastRollValues[getSum(currentRollValues) - 2] += 1;
+  sumCounts[getSum(currentRollValues) - 2] += 1;
 }
 
 void setup() {
@@ -42,10 +42,17 @@ void loop() {
     update();
   }
 
-  tinyfont.setCursor(1, 1);
-  tinyfont.print("NOW:" + getRollText(currentRollValues));
+  tinyfont.setCursor(0, 0);
+  tinyfont.print(getRollText(currentRollValues));
 
-  printRolls(1, 10, pastRollValues, tinyfont);
+  tinyfont.setCursor(0, 6);
+  tinyfont.print(getMaxSumCount(sumCounts));
+
+  drawGraph(
+    WIDTH - (11 * 8), 0,
+    sumCounts,
+    arduboy, tinyfont
+  );
 
   arduboy.display();
 }
--- arduino/2d6/display.h
+++ arduino/2d6/display.h
@@ -5,6 +5,17 @@ uint8_t getSum(uint8_t values[2]) {
   return values[0] + values[1];
 }
 
+// TODO: more idiomatic?
+uint8_t getMaxSumCount(int sumCounts[11]) {
+  uint8_t maxSum = 0;
+
+  for (uint8_t i = 0; i < 11; i++) {
+    maxSum = max(maxSum, sumCounts[i]);
+  }
+
+  return maxSum;
+}
+
 String getRollText(uint8_t values[2]) {
   return String(
     values[0]) + "+" + String(values[1]) +
@@ -14,13 +25,56 @@ String getRollText(uint8_t values[2]) {
 
 // TODO: arg types?
 // TODO: smaller, fix overlap when 3+ digits
-void printRolls(int x, int y, int pastRollValues[11], Tinyfont tinyfont) {
+void printRolls(int x, int y, int sumCounts[11], Tinyfont tinyfont) {
   for (uint8_t i = 0; i < 11; i++) {
     tinyfont.setCursor(x + i * 11, y);
     tinyfont.print(String(i + 2));
 
     tinyfont.setCursor(x + i * 11, y + 5);
-    tinyfont.print(String(pastRollValues[i]));
+    tinyfont.print(String(sumCounts[i]));
+  }
+}
+
+void drawGraph(
+  int x,
+  int y,
+
+  int sumCounts[11],
+
+  Arduboy2 arduboy,
+  Tinyfont tinyfont,
+
+  uint8_t width = 11 * 8,
+  uint8_t height = HEIGHT,
+
+  bool showFrame = true,
+  bool showLegend = true
+) {
+  if (showFrame) {
+    arduboy.drawRect(x, y, width, height);
+  }
+
+  uint8_t barWidth = width / 11;
+  int sumCount = getMaxSumCount(sumCounts);
+
+  for (uint8_t i = 0; i < 11; i++) {
+    uint8_t barHeight = (float(sumCounts[i]) / sumCount) * height;
+
+    if (barHeight > 0) {
+      arduboy.fillRect(
+        x + i * barWidth,
+        y + (height - barHeight),
+        barWidth,
+        barHeight,
+        1
+      );
+
+      if (showLegend) {
+        tinyfont.setCursor(x + i * barWidth, y + height - 5);
+        tinyfont.setTextColor(BLACK);
+        tinyfont.print(i + 2);
+      }
+    }
   }
 }
 
